b7106e5f48470a5d33137674bde59c08ea068444
Tom Szwaja <th.szwaja@gmail.com>
2021-12-06 15:22:15 +0100

    feat: add rotating sun

--- src/index.js
+++ src/index.js
@@ -1,15 +1,52 @@
+import { useRef } from "react";
 import ReactDOM from "react-dom";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import "./index.css";
+
+const objects = [];
+
+function Sphere() {
+    const radius = 1;
+    const widthSegments = 6;
+    const heightSegments = 6;
+    const meshRef = useRef(null);
+
+    const sphereMesh = (
+        <mesh ref={node => objects.push(node)} scale={[5, 5, 5]}>
+            <sphereBufferGeometry
+                args={[radius, widthSegments, heightSegments]}
+            />
+            <meshPhongMaterial emissive={0xffff00} />
+        </mesh>
+    );
+
+    return sphereMesh;
+}
+
+function Animate() {
+    useFrame(({ clock }) => {
+        if (objects.length === 0) return null;
+
+        const time = clock.getElapsedTime();
+        objects.forEach(obj => {
+            obj.rotation.y = time;
+        });
+    });
+
+    return null;
+}
 
 function App() {
     return (
         <div id="canvas-container">
-            <Canvas camera={{ fov: 90, position: [0, 1, 4] }}>
-                <ambientLight />
-                <mesh>
-                    <boxBufferGeometry />
-                    <meshStandardMaterial color="blue" />
+            <Canvas camera={{ fov: 90, position: [0, 1, 25] }}>
+                {/* <ambientLight /> */}
+                <pointLight args={[0xffffff, 3]} position={[0, 0, 0]} />
+                <Animate />
+                <Sphere />
+                <mesh position={[0, -10, 0]}>
+                    <boxBufferGeometry args={[2, 2, 2]}></boxBufferGeometry>
+                    <meshPhongMaterial />
                 </mesh>
             </Canvas>
         </div>
